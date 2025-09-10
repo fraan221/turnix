4038b0483e32d1bff667dd3f8fff8c9d6e85c0f0
Franco Herrera <franhe345@gmail.com>
2025-09-10 16:39:56 -0300

    fix(landing): solve some positions in hero section and cta section in mobile

--- components/landing/CtaSection.tsx
+++ components/landing/CtaSection.tsx
@@ -19,13 +19,13 @@ export function CtaSection() {
       <div className="absolute inset-0 z-10 bg-black/60" />
 
       <div className="container relative z-20 grid items-center justify-center gap-4 px-4 text-center md:px-6">
-        <div className="space-y-3">
+        <div className="flex flex-col items-center max-w-3xl gap-6 mx-auto text-center">
           <h2 className="text-3xl font-bold tracking-tighter text-white md:text-4xl/tight font-heading">
             ¿Empezamos?
           </h2>
-          <p className="mx-auto max-w-[600px] text-white/80 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-            14 días para probar todo gratis. Si no te convence, te vas sin pagar
-            nada.
+          <p className="text-white/80 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            14 días para probar todo gratis. <br /> Si no te convence, podes
+            dejar de usar la aplicación.
           </p>
         </div>
         <div className="flex flex-col items-center gap-4 mt-6">
--- components/landing/HeroSection.tsx
+++ components/landing/HeroSection.tsx
@@ -17,13 +17,15 @@ export function HeroSection() {
       <div className="absolute inset-0 z-10 bg-black/60" />
       <div className="container relative z-20 px-4 md:px-6">
         <div className="flex flex-col items-center gap-4 text-white">
-          <h1 className="text-4xl font-bold tracking-tighter font-heading sm:text-5xl md:text-6xl lg:text-7xl text-balance">
-            ¿Cuántos clientes perdiste hoy?
-          </h1>
-          <p className="max-w-[700px] text-white/80 md:text-xl text-balance">
-            Con Turnix, cada cliente que te busca, reserva. Sin vueltas, sin
-            pérdidas.
-          </p>
+          <div className="flex flex-col items-center max-w-3xl gap-6 mx-auto text-center">
+            <h1 className="text-4xl font-bold tracking-tighter font-heading sm:text-5xl md:text-6xl lg:text-7xl text-balance">
+              ¿Cuántos clientes perdiste hoy?
+            </h1>
+            <p className="max-w-[700px] text-white/80 md:text-xl text-balance">
+              Con Turnix, cada cliente que te busca, reserva. Sin vueltas, sin
+              pérdidas.
+            </p>
+          </div>
           <div className="flex flex-col items-center gap-4 mt-4">
             <Button asChild size="lg">
               <Link href="/register">Empezar Gratis</Link>
