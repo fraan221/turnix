6bf0152ef8854f36ec39e95c7e4d31b733a56ad0
Franco Herrera <franhe345@gmail.com>
2025-09-17 13:08:00 -0300

    fix(login & register): decomment lines of code refering google sign

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -64,7 +64,7 @@ export default function LoginForm() {
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-2xl">Inicia Sesión</CardTitle>
-          {/* <CardDescription>Ingresa con tu cuenta de Google</CardDescription> */}
+          <CardDescription>Ingresa con tu cuenta de Google</CardDescription>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit}>
@@ -77,7 +77,7 @@ export default function LoginForm() {
                 </Alert>
               )}
 
-              {/* <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-4">
                 <GoogleSignInButton />
               </div>
 
@@ -85,7 +85,7 @@ export default function LoginForm() {
                 <span className="relative z-10 px-2 bg-card text-muted-foreground">
                   O continúa con
                 </span>
-              </div> */}
+              </div>
 
               <div className="grid gap-4">
                 <div className="grid gap-2">
