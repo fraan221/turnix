36c91f5257cca4b030724608f250d88867cbcb8f
Herrera Franco <franhe345@gmail.com>
2026-02-25 13:35:30 -0300

    fix: implement internal routing for soketi backend connection

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,11 +1,15 @@
 import PusherServer from "pusher";
 
+const host = process.env.PUSHER_HOST_INTERNAL || process.env.NEXT_PUBLIC_PUSHER_HOST!;
+const port = process.env.PUSHER_PORT_INTERNAL || process.env.NEXT_PUBLIC_PUSHER_PORT!;
+const useTLS = (process.env.PUSHER_TLS_INTERNAL || process.env.NEXT_PUBLIC_PUSHER_TLS) === "true";
+
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
-  host: process.env.NEXT_PUBLIC_PUSHER_HOST!,
-  port: process.env.NEXT_PUBLIC_PUSHER_PORT!,
-  useTLS: process.env.NEXT_PUBLIC_PUSHER_TLS === "true",
+  host: host,
+  port: port,
+  useTLS: useTLS,
   cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER || "mt1",
 });
